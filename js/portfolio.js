0a66499cb538a36ed9eeb85ac0d8947813d3a0c0
Ethan <112027599+ethantl-1511@users.noreply.github.com>
2022-12-29 10:05:13 -0600

    Update portfolio.js
    fixes addEventListener

--- js/portfolio.js
+++ js/portfolio.js
@@ -7,36 +7,43 @@ function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
 
-// Slideshow code
-var slideIndex = 1;
-showSlides(slideIndex);
+// Closes the contact form when user clicks off it
+document.addEventListener("click", function(event) // document listens for a "click", then executes function(event)
+    {
+        if (event.target.matches(".cancel") ||
+            !event.target.closest(".Pop_Up_Button") &&
+            !event.target.closest(".form-popup") &&
+            !event.target.closest(".btn") &&
+            !event.target.closest(".contact"))
+        { // if you click on cancel, but DONT click on the others,
+            closeForm()
+        }
+    }, 
+    false )
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+// // Slideshow code, not used in final version
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
 
-function showSlides(n) {
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1};
-    if (n < 1) {slideIndex = slides.length};
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
 
-// Creates/closes the contact form when user clicks off it
-document.addEventListener("click", function(event){
-    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
-        closeForm()
-    }
-}, false )
+// function showSlides(n) {
+//     var slides = document.getElementsByClassName("mySlides");
+//     var dots = document.getElementsByClassName("dot");
+//     if (n > slides.length) {slideIndex = 1};
+//     if (n < 1) {slideIndex = slides.length};
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+//     dots[slideIndex - 1].className += " active";
+// }
